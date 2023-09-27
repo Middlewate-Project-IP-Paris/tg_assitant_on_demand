993b843ce6aded9f80fb5dce1a2c6f20622577fa
NikitaZernov184 <nzernov2001@gmail.com>
2023-09-27 16:13:13 +0200

    vars file

---
+++ vars.py
@@ -0,0 +1,6 @@
+CHAT_ID = -1001604616173
+TOKEN = '6569366772:AAEYXf01qD2atyNslTUkzksjBTtXOJfwhZc'
+TG_URL = 'https://api.telegram.org/bot'
+MEMBER_COUNT_METHOD = '/getChatMemberCount?chat_id='
+KAFKA_BROKER = 'localhost:9092'
+TOPIC_SUBS = 'todo'
