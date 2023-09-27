f67b7c1df21cd48aeeb2c190543565d988e8dfb5
NikitaZernov184 <nzernov2001@gmail.com>
2023-09-27 16:12:33 +0200

    sending to kafka added

--- bot_connection.py
+++ bot_connection.py
@@ -1,15 +1,35 @@
 import requests
 import telebot
 import json
+import vars
+from datetime import datetime
+import math
+from json import dumps
+from kafka import KafkaProducer
 
-chat_id = -1001604616173
-token = '6569366772:AAEYXf01qD2atyNslTUkzksjBTtXOJfwhZc'
+# response = requests.get(
+#    f'''{vars.TG_URL}{vars.TOKEN}{vars.MEMBER_COUNT_METHOD}{vars.CHAT_ID}''')
 
-response = requests.get(
-    f"https://api.telegram.org/bot{token}/getChatMemberCount?chat_id={chat_id}")
+# print(response.content)
 
-print(response.content)
+bot = telebot.TeleBot(vars.TOKEN)
 
-bot = telebot.TeleBot(token)
+# print(bot.get_chat_member_count(vars.CHAT_ID))
 
-print(bot.get_chat_member_count(chat_id))
+
+unit_dict = {
+    'timestamp': math.floor(datetime.timestamp(datetime.now())),
+    'channel_id': vars.CHAT_ID,
+    'number_of_followers': bot.get_chat_member_count(vars.CHAT_ID)
+}
+
+json_object = json.dumps(unit_dict)
+print(json_object)
+
+producer = KafkaProducer(bootstrap_servers=[vars.KAFKA_BROKER],
+                         value_serializer=lambda x:
+                         dumps(x).encode('utf-8'))
+
+producer.send(vars.TOPIC_SUBS, value=unit_dict)
+
+
