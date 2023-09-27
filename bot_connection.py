b8e4701b9b52b8a1ca211d0012cdbcaffe2de132
NikitaZernov184 <nzernov2001@gmail.com>
2023-09-27 16:42:59 +0200

    scheduler added

--- bot_connection.py
+++ bot_connection.py
@@ -1,4 +1,4 @@
-import requests
+# import requests
 import telebot
 import json
 import vars
@@ -6,6 +6,8 @@ from datetime import datetime
 import math
 from json import dumps
 from kafka import KafkaProducer
+import schedule
+import time
 
 # response = requests.get(
 #    f'''{vars.TG_URL}{vars.TOKEN}{vars.MEMBER_COUNT_METHOD}{vars.CHAT_ID}''')
@@ -17,19 +19,25 @@ bot = telebot.TeleBot(vars.TOKEN)
 # print(bot.get_chat_member_count(vars.CHAT_ID))
 
 
-unit_dict = {
-    'timestamp': math.floor(datetime.timestamp(datetime.now())),
-    'channel_id': vars.CHAT_ID,
-    'number_of_followers': bot.get_chat_member_count(vars.CHAT_ID)
-}
+def one_job():
+    unit_dict = {
+        'timestamp': math.floor(datetime.timestamp(datetime.now())),
+        'channel_id': vars.CHAT_ID,
+        'number_of_followers': bot.get_chat_member_count(vars.CHAT_ID)
+    }
 
-json_object = json.dumps(unit_dict)
-print(json_object)
+    json_object = json.dumps(unit_dict)
+    print(json_object)
 
-producer = KafkaProducer(bootstrap_servers=[vars.KAFKA_BROKER],
-                         value_serializer=lambda x:
-                         dumps(x).encode('utf-8'))
+    producer = KafkaProducer(bootstrap_servers=[vars.KAFKA_BROKER],
+                             value_serializer=lambda x:
+                             dumps(x).encode('utf-8'))
 
-producer.send(vars.TOPIC_SUBS, value=unit_dict)
+    producer.send(vars.TOPIC_SUBS, value=unit_dict)
 
 
+schedule.every(10).minutes.do(one_job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
