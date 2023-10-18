e111630ebde21c9c442626151f952a9fd6777df1
NikitaZernov184 <nzernov2001@gmail.com>
2023-10-18 15:59:27 +0200

    files for logic and server

--- server.py
+++ server.py
@@ -11,22 +11,27 @@ class AssistanceOnDemandServicer(assistant_on_demand_pb2_grpc.assistanceOnDemand
 
     def getChannelMeta(self, request, context):
         b = BotConnection()
+        print('getChannelMeta invoked \n')
         b.getchatmembers(request.channel_id)
 
     def getPost(self, request, context):
         b = BotConnection()
+        print('getPost invoked \n')
         b.getpost(request.channel_id, request.post_id)
 
     def getChannelSubs(self, request, context):
         b = BotConnection()
+        print('getChannelSubs invoked \n')
         b.getchatmembers(request.channel_id)
 
     def getPostStat(self, request, context):
         b = BotConnection()
+        print('getPostStat invoked \n')
         b.getpost(request.channel_id, request.post_id)
 
     def joinChannel(self, request, context):
         b = BotConnection()
+        print('joinChannel invoked \n')
         b.getchatmembers(request.invite_link)
 
 
